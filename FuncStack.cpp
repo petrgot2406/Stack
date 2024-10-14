fabe621f172af3b7be2db08147719bf5c0acb264
petrgot2406 <p3trgot@yandex.ru>
2024-10-15 00:36:33 +0300

    Corrected code style

--- FuncStack.cpp
+++ FuncStack.cpp
@@ -8,7 +8,8 @@ void PushStack(Stack_t* stk, int n)
 {
     if (stk->params.size + 1 > stk->params.capacity)
     {
-        stk->stkdata.data = (int*)realloc(stk->stkdata.data, 2 * stk->params.size * sizeof(int));
+        stk->stkdata.data = (int*)realloc(stk->stkdata.data,
+                                          2 * stk->params.size * sizeof(int));
     }
     stk->stkdata.data[stk->params.size] = n;
     stk->params.size++;
@@ -18,7 +19,8 @@ int PopStack(Stack_t* stk)
 {
     if (4 * (stk->params.size - 1) < stk->params.capacity)
     {
-        stk->stkdata.data = (int*)realloc(stk->stkdata.data, stk->params.size * sizeof(int) / 2);
+        stk->stkdata.data = (int*)realloc(stk->stkdata.data,
+                                          stk->params.size * sizeof(int) / 2);
     }
     printf("I popped %d\n", stk->stkdata.data[stk->params.size - 1]);
     int temp = stk->stkdata.data[stk->params.size];
